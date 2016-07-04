9fddbc5a85af292ffd46827063675f7678c0f0f3
Priya <priya.pancholi@promactinfo.com>
2016-07-04 18:46:02 +0530

    mvc demo app

---
+++ MVCdemo/Controllers/movieController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCdemo.Models;
+
+
+namespace MVCdemo.Controllers
+{
+    public class movieController : Controller
+    {
+        // GET: movie
+        public ActionResult movieview()
+        {
+            var moviemodel = new moviemodel()
+            {
+                id = 1,
+                name = "rustom"
+            };
+            return View(moviemodel);
+        }
+    }
+}
---
+++ MVCdemo/Models/moviemodel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCdemo.Models
+{
+    public class moviemodel
+    {
+        public int id
+        { get; set; }
+
+        public String name
+        { get; set; }
+
+    }
+}
